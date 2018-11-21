d05e773d025cb5b4470b234ac40515b8f7b86f56
Rove Monteux <rmonteux@centrum.cz>
2018-11-21 11:34:33 +0100

    Added support for Java 11

--- src/cf/monteux/intelligence/network/Https.java
+++ src/cf/monteux/intelligence/network/Https.java
@@ -30,22 +30,63 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package cf.monteux.intelligence.network;
 
+import cf.monteux.intelligence.properties.Agent;
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
-
-import org.apache.commons.io.IOUtils;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
 
 public class Https {
 
+    public Https() {
+
+    }
+
+    public ArrayList<String> addresses = new ArrayList<>();
+
+    // Java 11
+    public HttpResponse<String> getResponse(String address) throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(Version.HTTP_2)
+                .build();
+        HttpRequest mainRequest = HttpRequest.newBuilder()
+                .uri(URI.create("address"))
+                .setHeader("User-Agent", Agent.get())
+                .build();
+        HttpResponse<String> mainResponse = httpClient.send(mainRequest, BodyHandlers.ofString());
+        return mainResponse;
+    }
+
+    // Java 11
+	public void topResults(String term) throws IOException, InterruptedException {
+		// Google, Facebook, Instagram, LinkedIn, Bing, DuckDuckGo
+        term = term.replace(" ", "%20");
+        HttpResponse<String> duckDuckGo = getResponse("https://duckduckgo.com/html?q="+term);
+        //  <a rel="nofollow" class="result__a" href="https://www.youtube.com/channel/UCGGMaCGq42mBm5eazzol6Bg"><b>Rove</b> <b>Monteux</b> - YouTube</a>
+        duckDuckGo.body().lines()
+                .filter(line -> line.contains("result__a"))
+                .map(line -> line.substring(line.indexOf("href='") + 5, line.indexOf("'>")))
+                .forEach(result -> {
+                    addresses.add(result); });
+    }
+
+    // Java 6
 	public static String retrieve(String address) throws IOException {
 		String charset = "UTF-8";
 
 	    URL url = new URL(address);
 	    URLConnection hc = url.openConnection();
-        hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+        hc.setRequestProperty("User-Agent", Agent.get());
 	    Reader reader = new InputStreamReader(hc.getInputStream(), charset);
 	    return IOUtils.toString(reader);
 	}
